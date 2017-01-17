1773ba29d350e1ea4dc1248ea363747020702d70
kevin <k.charbz5@gmail.com>
2017-01-16 20:57:33 -0500

    added all files

---
+++ imports/components/todosList/todosList.js
@@ -0,0 +1,23 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import { Tasks } from '../../api/tasks.js';
+import template from './todosList.html';
+
+class TodosListCtrl {
+  constructor($scope) {
+    $scope.viewModel(this);
+
+    this.helpers({
+      tasks() {
+        return Tasks.find({});
+      }
+    })
+  }
+}
+
+...some lines skipped...
+])
+  .component('todosList', {
+    templateUrl: 'imports/components/todosList/todosList.html',
+    controller: ['$scope', TodosListCtrl]
+  });
