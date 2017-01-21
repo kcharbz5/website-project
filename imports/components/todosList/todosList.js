543d4bfcc24468d63c83374a2653c515b04130db
kevin <k.charbz5@gmail.com>
2017-01-21 09:58:03 -0500

    added tasks per user

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -1,6 +1,7 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import template from './todosList.html';
+import { Meteor } from 'meteor/meteor';
 import { Tasks } from '../../api/tasks.js';
 
 class TodosListCtrl {
@@ -31,7 +32,9 @@ class TodosListCtrl {
             $ne: true
           }
         }).count();
-      }
+      },
+      currentUser() {
+        return Meteor.user();
     })
   }
 
@@ -39,7 +42,9 @@ class TodosListCtrl {
    // Insert a task into the collection
    Tasks.insert({
      text: newTask,
-     createdAt: new Date
+     createdAt: new Date,
+     owner: Meteor.userId(),
+     username: Meteor.user().username
    });
    // Clear form
    this.newTask = '';
